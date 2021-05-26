058d0b0624af62aca65936c785f9c0efe4bcee52
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-26 19:08:49 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.a48f0df9c02d0069187e3ba51815b34c.js"
 );
 
 self.addEventListener('message', (event) => {
